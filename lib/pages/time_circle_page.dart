59c352f27972e8c0946900d4b2b90a172a74de85
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-06-15 23:20:56 +0700

    add time circle file

--- lib/pages/time_circle_page.dart
+++ lib/pages/time_circle_page.dart
@@ -1,1 +1,13 @@
 
+import 'package:flutter/material.dart';
+
+class TimeCircles extends StatelessWidget {
+  const TimeCircles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.amber,
+    );
+  }
+}
